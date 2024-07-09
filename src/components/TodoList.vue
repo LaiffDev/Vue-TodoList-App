498459a49689d2452d48671bf578e59eb4236dd9
LaiffDev <laiffdev@gmail.com>
2024-07-09 09:36:03 +0200

    implemented priorities

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,21 +3,38 @@ export default{
     data(){
         return {
             tasks : [],
-            taskInfo : ''
+            taskInfo : '',
+            selectedPriority : ''
         }
     },
     methods:{
-        getTasks(){
+        getTasks() {
             this.taskInfo = this.$refs.taskRef.value
-            this.tasks.push(this.taskInfo)
+            
+            if (this.taskInfo && this.selectedPriority) {
+                this.tasks.push({ description: this.taskInfo, priority: this.selectedPriority })
+                localStorage.setItem('tasks', JSON.stringify(this.tasks))
+            }
             
             this.$refs.taskRef.value = ''
+            this.selectedPriority = ''
         },
-        deleteTask(){
-            //delete task clicked
-
+        saveTasks() {
+            const savedTasks = localStorage.getItem('tasks')
+            if (savedTasks) {
+                this.tasks = JSON.parse(savedTasks)
+            }
+        },
+        deleteTask(index) {
+            const spliced = this.tasks.splice(index, 1)
+            localStorage.setItem('tasks', JSON.stringify(this.tasks))
+            console.log("deleted task: ", spliced)
         }
+    },
+    mounted(){
+        this.saveTasks()
     }
+
 }
 </script>
 
@@ -28,20 +45,56 @@ export default{
     </div>
     <div class="flex justify-center gap-5">
         <input type="text" ref="taskRef" class="border border-black p-2">
-        <button @click="getTasks" class="bg-green-400 p-2 ">Add Task</button>
+        <select name="priority" id="priority" v-model="selectedPriority" required>
+            <optgroup label="Priorities">
+                <option value="" disabled selected>Choose a priority</option>
+                <option value="high">High priority</option>
+                <option value="medium">Medium priority</option>
+                <option value="low">Low priority</option>
+            </optgroup>
+        </select>
+        <button @click="getTasks" class="bg-green-500 p-2 text-white">Add Task</button>
     </div>
-    <div class="w-fit m-auto mt-10 bg-slate-300">
-        <p>Todo Task Here : </p>
+    <div class="w-full m-auto mt-10 p-3">
+        <p class=" text-center">Todo Tasks : </p>
         <div>
-            <ul v-for="task of tasks" :key="task" class="flex gap-5 justify-between items-center m-5 text-center p-5">
-                <li>{{ task }}</li>
-                <li>
-                    <div>
-                        <button @click="deleteTask(task)" class="bg-red-500 p-2 text-white">Delete task</button>
+            <ul v-for="(task,index) of tasks" :key="index" class="flex gap-5 justify-between items-center text-center p-5">
+                <div class="bg-black rounded-full text-orange-500 flex gap-20 justify-between items-center w-full p-5">
+                    <p class="text-black" :class="{'high' : task.priority === 'high', 'medium' : task.priority === 'medium', 'low' : task.priority === 'low'}"></p>
+                    <p class="font-bold">{{ task.description }}</p>
+                    <div class="gap-5 flex">
+                        <button @click="deleteTask(index)" class="bg-red-500 p-2 text-white rounded-full">Delete task</button>
+                        <!-- <button  class="bg-blue-500 p-2 text-white rounded-full">Edit task</button> -->
                     </div>
-                </li>
- 
+                </div>
             </ul>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .high{
+        background-color: red;
+        padding: 2px;
+        border-radius: 100%;
+        color: white;
+        width:20px;
+        height: 20px;
+    }
+    .medium{
+        background-color: orange;
+        padding: 2px;
+        border-radius: 100%;
+        color: white;
+        width:20px;
+        height: 20px;
+    }
+    .low{
+        background-color: royalblue;
+        padding: 2px;
+        border-radius: 100%;
+        color: white;
+        width:20px;
+        height: 20px;
+    }
+</style>
